194e45b084ef97b9492481b0e715b843dbabbaa9
mhengerer <112892304+mhengerer@users.noreply.github.com>
2022-11-04 22:23:21 -0400

    almost finished javascript and did some more styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,117 +1,126 @@
 var timerEl = document.querySelector("#time");
 var startButton = document.querySelector("#start");
+var startScreen = document.querySelector("#start-screen");
+var endScreenEl = document.querySelector("#end-screen");
+var submitButton = document.querySelector('#submit');
 
+var quizEl = document.querySelector("#questions");
 var questionEl = document.querySelector("#question-title");
 var answerEl = document.querySelector("#choices");
+var messageEl = document.querySelector("#message");
+var finalScoreEl = document.querySelector("#final-score");
+var initialsEl = document.querySelector("#initials");
 
 var timer;
 var timeLeft;
 
 var randomQuestions;
-var currentQuestion; 
+var currentQuestion;
 
 var questions = [
-    {
-        question: 'whats 19 x 4215?',
-        answers: [
-            {text: '45654', correct: false},
-            {text: '93543', correct: false},
-            {text: '80085', correct: true},
-            {text: '73336', correct: false}
-        ]
-    },
-    {
-        question: 'what?',
-        answers: [
-            {text: 'scrip', correct: false},
-            {text: 'scramp', correct: false},
-            {text: 'scromp', correct: true},
-            {text: 'slamp', correct: false}
-        ]
-    },
-    {
-        question: 'when?',
-        answers: [
-            {text: 'blip', correct: false},
-            {text: 'blop', correct: false},
-            {text: 'bourg', correct: true},
-            {text: 'bromp', correct: false}
-        ]
-    },
-    {
-        question: 'where?',
-        answers: [
-            {text: 'grom', correct: false},
-            {text: 'grim', correct: false},
-            {text: 'grem', correct: true},
-            {text: 'glomp', correct: false}
-        ]
-    },
-    {
-        question: 'how?',
-        answers: [
-            {text: '45asd', correct: false},
-            {text: '93asdfa', correct: false},
-            {text: '80asdf', correct: true},
-            {text: '73asdf', correct: false}
-        ]
-    },
-    {
-        question: 'why?',
-        answers: [
-            {text: '45ffff654', correct: false},
-            {text: '9asdf3543', correct: false},
-            {text: '800asdfa85', correct: true},
-            {text: '73336', correct: false}
-        ]
-    },
-    {
-        question: 'for what reason?',
-        answers: [
-            {text: '456asdf54', correct: false},
-            {text: '93543', correct: false},
-            {text: '800asdf85', correct: true},
-            {text: '73336', correct: false}
-        ]
-    },
-    {
-        question: 'whom?',
-        answers: [
-            {text: '45asdf654', correct: false},
-            {text: '93543', correct: false},
-            {text: '80085', correct: true},
-            {text: '73336', correct: false}
-        ]
-    },
-    {
-        question: 'where art tho?',
-        answers: [
-            {text: '45654', correct: false},
-            {text: '935sdaf43', correct: false},
-            {text: '80085', correct: true},
-            {text: '73336', correct: false}
-        ]
-    },
-    {
-        question: 'gwen stefani?',
-        answers: [
-            {text: '45654', correct: false},
-            {text: '93543', correct: false},
-            {text: 'asd', correct: true},
-            {text: '73336', correct: false}
-        ]
-    }
-]
-
+  {
+    question: "whats 19 x 4215?",
+    answers: [
+      { text: "45654", correct: false },
+      { text: "93543", correct: false },
+      { text: "80085", correct: true },
+      { text: "73336", correct: false },
+    ],
+  },
+  {
+    question: "what?",
+    answers: [
+      { text: "scrip", correct: false },
+      { text: "scramp", correct: false },
+      { text: "scromp", correct: true },
+      { text: "slamp", correct: false },
+    ],
+  },
+  {
+    question: "when?",
+    answers: [
+      { text: "blip", correct: false },
+      { text: "blop", correct: false },
+      { text: "bourg", correct: true },
+      { text: "bromp", correct: false },
+    ],
+  },
+  {
+    question: "where?",
+    answers: [
+      { text: "grom", correct: false },
+      { text: "grim", correct: false },
+      { text: "grem", correct: true },
+      { text: "glomp", correct: false },
+    ],
+  },
+  {
+    question: "how?",
+    answers: [
+      { text: "45asd", correct: false },
+      { text: "93asdfa", correct: false },
+      { text: "80asdf", correct: true },
+      { text: "73asdf", correct: false },
+    ],
+  },
+  {
+    question: "why?",
+    answers: [
+      { text: "45ffff654", correct: false },
+      { text: "9asdf3543", correct: false },
+      { text: "800asdfa85", correct: true },
+      { text: "73336", correct: false },
+    ],
+  },
+  {
+    question: "for what reason?",
+    answers: [
+      { text: "456asdf54", correct: false },
+      { text: "93543", correct: false },
+      { text: "800asdf85", correct: true },
+      { text: "73336", correct: false },
+    ],
+  },
+  {
+    question: "whom?",
+    answers: [
+      { text: "45asdf654", correct: false },
+      { text: "93543", correct: false },
+      { text: "80085", correct: true },
+      { text: "73336", correct: false },
+    ],
+  },
+  {
+    question: "where art tho?",
+    answers: [
+      { text: "45654", correct: false },
+      { text: "935sdaf43", correct: false },
+      { text: "80085", correct: true },
+      { text: "73336", correct: false },
+    ],
+  },
+  {
+    question: "gwen stefani?",
+    answers: [
+      { text: "45654", correct: false },
+      { text: "93543", correct: false },
+      { text: "asd", correct: true },
+      { text: "73336", correct: false },
+    ],
+  },
+];
 
 // function to start game
 function startQuiz() {
-  timeLeft = 80;
+  timeLeft = 150;
   startButton.disabled = true;
-  randomQuestions = questions.sort(function() {return Math.random() - .5})
+  randomQuestions = questions.sort(function () {
+    return Math.random() - 0.5;
+  });
   console.log(randomQuestions);
   currentQuestion = 0;
-  startButton.classList.add('hide')
+  startScreen.classList.add("hide");
+  quizEl.classList.remove("hide");
   startTimer();
   // add call to function that renders question
   goToNextQuestion();
@@ -119,15 +128,60 @@ function startQuiz() {
 
 // function to go to next question
 function goToNextQuestion() {
-    renderQuestion(randomQuestions[currentQuestion])
+  renderQuestion(randomQuestions[currentQuestion]);
 }
 
 function renderQuestion(question) {
-    questionEl.textContent = question.question
+  questionEl.textContent = question.question;
+  console.log(question.answers);
+  answerEl.textContent = " ";
+  question.answers.forEach((answer) => {
+    var choiceButton = document.createElement("button");
+    choiceButton.textContent = answer.text;
+    choiceButton.classList.add("btn");
+    if (answer.correct) {
+      choiceButton.dataset.correct = answer.correct;
+    }
+    answerEl.appendChild(choiceButton);
+    choiceButton.addEventListener("click", pickTheAnswer);
+  });
 }
 
 // function for selection answer
 function pickTheAnswer() {
+  var dataCorrect = this.dataset.correct;
+
+  if (dataCorrect) {
+    messageEl.textContent = "Correct";
+  } else {
+    messageEl.textContent = "WRONG";
+    timeLeft = timeLeft - 10;
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+    }
+  }
+  currentQuestion++;
+  if (currentQuestion < questions.length) {
+    goToNextQuestion();
+  } else {
+    endScreen();
+  }
+}
+
+function endScreen() {
+  clearInterval(timer);
+  quizEl.classList.add("hide");
+  endScreenEl.classList.remove("hide");
+  finalScoreEl.textContent = timeLeft;
+}
+
+function saveHighScore(event) {
+    var initials = initialsEl.value
+    console.log(initials);  
+    var highScore = [initials, timeLeft]
+    console.log(highScore);
+    localStorage.setItem('High-Scores', highScore)
+    initialsEl.value = '';
 
 }
 
@@ -147,3 +201,7 @@ function startTimer() {
 
 // event listerner to start the game
 startButton.addEventListener("click", startQuiz);
+
+// event listener for saving highscores
+submitButton.addEventListener('click', saveHighScore)
+
